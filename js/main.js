7a18a80244c4ff39cbce596bf26a1499045b5347
myriahnottage <myriah.nottage@gmail.com>
2021-07-27 20:40:10 -0700

    form grid

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-console.log(document.forms);
-console.log(document.forms[0]);
-console.log(document.forms['ask'].question);
-console.log(document.forms['ask'].question.value);
+// console.log(document.forms);
+// console.log(document.forms[0]);
+// console.log(document.forms['ask'].question);
+// console.log(document.forms['ask'].question.value);
+
+const form = document.forms.ask;
+form.addEventListener('submit', function (event){
+    event.preventDefault();
+    // prevents the default behavior (form behavior is to send data to a server)
+    console.log(form.question.value);
+    form.reset();
+})
